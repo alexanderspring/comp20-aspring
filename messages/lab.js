1a9fd09eefc34dc020e6811b5ddefe1d74cb88ea
Alexander Spring <Alex@Alexanders-MacBook-Pro-4.local>
2015-10-21 23:15:10 -0400

    code completed

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,18 @@
-// Your JavaScript goes here...
+	
+	var message = new XMLHttpRequest();
+
+	function parse(){
+		message.open("GET", "data.json", true)
+		message.send(null);
+		message.onreadystatechange = function(){
+			if(message.readyState == 4){
+				data = message.responseText;
+				parsedData = JSON.parse(data);
+				elem = document.getElementById("messages");
+				for (count = 0; count < parsedData.length; count++){
+           			elem.innerHTML += "<p>" + parsedData[count]["content"] + " - " 
+          			 + parsedData[count]["username"] + "</p>";
+				}
+			}
+		}
+	}
